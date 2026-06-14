a7d42fed8512ac0d6d8d4023b1c46e5800b6359e
Roshan Yadav <yadavroshanarvind2004@gmail.com>
2026-06-14 20:59:18 +0530

    Prepare notification service for deployment

--- Mircoservices/notification-service/src/main/java/com/jobportal/notification_service/dto/ApplicationStatusRequest.java
+++ Mircoservices/notification-service/src/main/java/com/jobportal/notification_service/dto/ApplicationStatusRequest.java
@@ -20,8 +20,4 @@ public class ApplicationStatusRequest {
     private String jobTitle;
 
     private ApplicationStatus status;
-
-    public String getTo() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
 }
