8769c3c5ae6df2d557013a4654320c519713d461
Roshan Yadav <yadavroshanarvind2004@gmail.com>
2026-06-24 21:42:35 +0530

    Prepare notification service for deployment

--- backend/src/services/smtpTransporter.js
+++ backend/src/services/smtpTransporter.js
@@ -1,7 +1,12 @@
+const dns = require('dns');
+const net = require('net');
+const tls = require('tls');
 const nodemailer = require('nodemailer');
 
 const env = require('../config/env');
 const logger = require('../config/logger');
+
+dns.setDefaultResultOrder('ipv4first');
 
 let transporter = null;
 
@@ -13,6 +18,57 @@ function isSmtpConfigured() {
         env.MAIL_PASSWORD &&
         env.MAIL_FROM,
     );
+}
+
+function createIpv4SmtpSocket(options, callback) {
+    dns.lookup(options.host, { family: 4 }, (dnsError, address, family) => {
+        if (dnsError) {
+            logger.error('SMTP IPv4 DNS resolution failed', {
+                smtp_host: options.host,
+                error_message: dnsError.message,
+            });
+            return callback(dnsError);
+        }
+
+        logger.info('SMTP host resolved to IPv4 address', {
+            smtp_host: options.host,
+            smtp_ip: address,
+            family,
+        });
+
+        const socketOptions = {
+            host: address,
+            port: options.port,
+            family: 4,
+            servername: options.host,
+        };
+
+        if (options.localAddress) {
+            socketOptions.localAddress = options.localAddress;
+        }
+
+        const socket = options.secure
+            ? tls.connect(
+                  {
+                      ...socketOptions,
+                      ...(options.tls || {}),
+                  },
+                  () => {
+                      socket.removeListener('error', onSocketError);
+                      callback(null, { connection: socket, secured: true });
+                  },
+              )
+            : net.connect(socketOptions, () => {
+                  socket.removeListener('error', onSocketError);
+                  callback(null, { connection: socket, secured: false });
+              });
+
+        function onSocketError(socketError) {
+            callback(socketError);
+        }
+
+        socket.once('error', onSocketError);
+    });
 }
 
 function getTransporter() {
@@ -32,6 +88,8 @@ function getTransporter() {
             connectionTimeout: 30000,
             greetingTimeout: 30000,
             socketTimeout: 30000,
+            family: 4,
+            getSocket: createIpv4SmtpSocket,
         });
     }
 
